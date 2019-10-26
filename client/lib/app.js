86f9bdd6bf5a6bbe2ac15ddad4da74fba475c7e0
Michael Kraus <mike.kraus@onlinehome.de>
2019-10-26 17:21:32 +0200

    hint where to put prezi id

--- client/lib/app.js
+++ client/lib/app.js
@@ -1,11 +1,5 @@
 
 
-
-// event handlers for the buttons
-// document.getElementById("b-previous").addEventListener('click', function(){
-//     player.previousStep();
-// });
-
 document.getElementById("b-next").addEventListener('click', function(){
 	document.getElementsByClassName('webgl-viewer-navbar-button webgl-viewer-navbar-button-next')[0].click();
 	document.getElementsByClassName('webgl-viewer-navbar-button webgl-viewer-navbar-button-next')[0].click();
