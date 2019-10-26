dc6c51616490a2b71f3124a01fc11ca6db911ff4
Michael Kraus <mike.kraus@onlinehome.de>
2019-10-26 17:19:31 +0200

    some fixes on prezi html

--- client/lib/app.js
+++ client/lib/app.js
@@ -1,34 +1,16 @@
 
-// initialize the player object
-var player = new PreziPlayer('prezi-player', {
-	preziId: "xzrf8_apklcr",
-	width: 500,
-	height: 400,
-	debug: true
-});
 
-// if content is ready, get and display the title of the prezi and number of steps
-player.on(PreziPlayer.EVENT_STATUS, function(event) {
-	if (event.value == PreziPlayer.STATUS_CONTENT_READY) {
-		document.getElementById('i-title').value = player.getTitle();
-		setTimeout(function(){
-			document.getElementById('t-step-count').innerHTML = player.getStepCount();
-		}, 2000);	
-	}
-});
-
-// display the current step
-player.on(PreziPlayer.EVENT_CURRENT_STEP, function(event) {
-	document.getElementById('t-current-step').innerHTML = event.value + 1;
-});
 
 // event handlers for the buttons
-document.getElementById("b-previous").addEventListener('click', function(){
-    player.previousStep();
-});
+// document.getElementById("b-previous").addEventListener('click', function(){
+//     player.previousStep();
+// });
+
 document.getElementById("b-next").addEventListener('click', function(){
-    player.nextStep();
+	document.getElementsByClassName('webgl-viewer-navbar-button webgl-viewer-navbar-button-next')[0].click();
+	document.getElementsByClassName('webgl-viewer-navbar-button webgl-viewer-navbar-button-next')[0].click();
+	document.getElementsByClassName('webgl-viewer-navbar-button glowing webgl-viewer-navbar-button-next')[0].click();	
 });
-document.getElementById("b-move-to-step").addEventListener('click', function(){
-    player.toStep(document.getElementById("i-step-number").value - 1);
-});
+// document.getElementById("b-move-to-step").addEventListener('click', function(){
+//     player.toStep(document.getElementById("i-step-number").value - 1);
+// });
